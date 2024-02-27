d46bee47cb6590bfd2d4bf08c4c26f1ed9b0b67d
Maxime Ndutiye <maximenduti@gmail.com>
2024-02-27 11:25:17 -0500

    use spin default dotfiles

--- config/nvim/init.vim
+++
@@ -1,3 +0,0 @@
-set runtimepath^=~/.vim runtimepath+=~/.vim/after
-set packpath+=~/.vim
-source ~/.vimrc
---
+++ personal/install.sh
@@ -0,0 +1,12 @@
+# Custom actions to take on initial install of dotfiles.
+# This runs after default install actions, so you can overwrite changes it makes if you want.
+
+# Install AstroNvim
+mv ~/.config/nvim ~/.config/nvim.bak
+
+mv ~/.local/share/nvim ~/.local/share/nvim.bak
+mv ~/.local/state/nvim ~/.local/state/nvim.bak
+mv ~/.cache/nvim ~/.cache/nvim.bak
+
+git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+
