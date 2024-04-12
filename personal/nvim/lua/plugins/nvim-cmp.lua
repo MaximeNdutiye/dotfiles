b772dc7903ecab53151b8b162c01a0b4d5805eac
Maxime Ndutiye <maxime.ndutiye@shopify.com>
2024-04-12 10:20:00 -0400

    disable plugins in vscode

--- personal/nvim/lua/plugins/nvim-cmp.lua
+++ personal/nvim/lua/plugins/nvim-cmp.lua
@@ -5,27 +5,27 @@ return {
   opts = function(_, opts)
     -- opts parameter is the default options table
     -- the function is lazy loaded so cmp is able to be required
-    local cmp = require "cmp"
-    local lspkind = require("lspkind")
+    -- local cmp = require "cmp"
+    -- local lspkind = require("lspkind")
 
     -- modify the sources part of the options table
-    opts.sources = cmp.config.sources {
-      { name = "copilot",  group_index = 2 }, --add copilot
-      { name = "nvim_lsp", priority = 1000 },
-      { name = "luasnip",  priority = 750 },
-      { name = "buffer",   priority = 500 },
-      { name = "path",     priority = 250 },
-    }
+    -- opts.sources = cmp.config.sources {
+    --   { name = "copilot",  group_index = 2 }, --add copilot
+    --   { name = "nvim_lsp", priority = 1000 },
+    --   { name = "luasnip",  priority = 750 },
+    --   { name = "buffer",   priority = 500 },
+    --   { name = "path",     priority = 250 },
+    -- }
 
     -- add copilot icon to lspkind
-    opts.formatting = {
-      format = lspkind.cmp_format({
-        mode = "symbol",
-        max_width = 50,
-        symbol_map = { Copilot = "" }
-      })
-    }
+    -- opts.formatting = {
+    --   format = lspkind.cmp_format({
+    --     mode = "symbol",
+    --     max_width = 50,
+    --     symbol_map = { Copilot = "" }
+    --   })
+    -- }
     -- return the new table to be used
-    return opts
+    -- return opts
   end,
 }
